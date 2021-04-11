1b7d8c46e3ef2dea3f3f6955b0b43827b54bf2ae
RebeccaRoach <rebeccaroach12@gmail.com>
2021-04-11 16:19:28 -0700

    Header and fontawesome cdn

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <img src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"/>
+        <h2>Header 1</h2>
+        <h2>Header 2</h2>
+        <div className="header__verticalLine"></div>
+        <div>Esports</div>
+      </div>
+
+      <div className='header__center'>
+
+      </div>
+
+      <div className='header__right'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
