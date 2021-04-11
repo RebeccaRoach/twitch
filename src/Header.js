e6e5113bf26720d5c549d0a5038ea42d050b8fef
RebeccaRoach <rebeccaroach12@gmail.com>
2021-04-11 16:32:35 -0700

    Center header

--- src/Header.js
+++ src/Header.js
@@ -4,19 +4,23 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='header__left'>
-        <img src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"/>
-        <h2>Header 1</h2>
-        <h2>Header 2</h2>
+        <img src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"
+        alt="twitch logo"/>
+        <h2>Following</h2>
+        <h2>Browse</h2>
         <div className="header__verticalLine"></div>
-        <div>Esports</div>
+        <h2>Esports</h2>
+        <h2>Music</h2>
+        <i class="fas fa-ellipsis-h"></i>
       </div>
-
       <div className='header__center'>
-
+        <input className="text" placeholder="Search"/>
+          <div className="header__centerLogoContainer">
+            <i class="fas fa-search"></i>
+          </div>
       </div>
-
       <div className='header__right'>
-
+        
       </div>
     </div>
   )
