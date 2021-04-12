a85323aeb24d606847c6dac9e12a5ddc23db8e9a
RebeccaRoach <rebeccaroach12@gmail.com>
2021-04-11 17:19:52 -0700

    Header styles progress

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import './Header.css';
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header__left'>
-        <img src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"
-        alt="twitch logo"/>
+        <img className='header__leftLogo' src="https://pngimg.com/uploads/twitch/twitch_PNG6.png"
+          alt="twitch logo"/>
         <h2>Following</h2>
         <h2>Browse</h2>
         <div className="header__verticalLine"></div>
@@ -13,21 +15,25 @@ const Header = () => {
         <h2>Music</h2>
         <i class="fas fa-ellipsis-h"></i>
       </div>
+
       <div className='header__center'>
         <input className="text" placeholder="Search"/>
           <div className="header__centerLogoContainer">
             <i class="fas fa-search"></i>
           </div>
       </div>
+
       <div className='header__right'>
         <div className="header__rightLogoContainer">
           <i class="fas fa-crown"></i>
           <i class="fas fa-inbox"></i>
           <i class="fas fa-comment-alt"></i>
+          
         <div className="header__rightBits">
           <i class="fas fa-gem"></i>
           <h4>Get Bits</h4>
         </div>
+        <Avatar/>
       </div>
     </div>
   </div>
