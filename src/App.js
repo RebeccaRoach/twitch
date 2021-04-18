607b24a6b8ca747a55ab62bced11299210c80b15
RebeccaRoach <rebeccaroach12@gmail.com>
2021-04-18 16:32:09 -0700

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
  import './App.css';
  import Header from './Header';
+ import SideBar from './SideBar';
 
 function App() {
   return (
     <div className="app">
      <Header/>
-     <div>APP</div>
+     <SideBar />
     </div>
   );
 }
