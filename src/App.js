7ed8268b520a14ed40946295cf7e7d744e79de09
RebeccaRoach <rebeccaroach12@gmail.com>
2021-04-18 17:00:57 -0700

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
  import './App.css';
- import Header from './Header';
+//  import Header from './Header';
  import SideBar from './SideBar';
 
 function App() {
   return (
     <div className="app">
-     <Header/>
-     <SideBar />
+      <Header />
+      <SideBar />
     </div>
   );
 }
